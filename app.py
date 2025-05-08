4e35c1e60589547fbe0e126030c893fbfbf6e155
d-beech <datubeechmusic@gmail.com>
2025-05-09 10:07:28 +1200

    Added tempurature settings to Llama payload

--- app.py
+++ app.py
@@ -3,47 +3,24 @@ import requests
 
 app = Flask(__name__)
 
-token_length = (128, 256, 512)
+token_length = (32, 256, 512)
 vocab_complexity = ('very simple, child level', '15 year old level', 'university level')
-tone = ('friendly', 'aggressive', 'formal')
+tone = ('friendly', 'extremely aggressive', 'formal')
 
 
-#system_prompt =f'''You are an educational chatbot called Juan, respond using a {tone(1)} tone. 
-#Respond using {vocab_complexity(2)} vocabulary. Do not talk about Pokemon'''
+SYSTEM_PROMPT =f'''You are an educational chatbot called Juan, respond using a {tone[0]} tone. 
+#Respond using {vocab_complexity[2]} vocabulary. Do not talk about Pokemon'''
 
 llama_endpoint = "http://127.0.0.1:11434/api/chat"
 
-
-
 LLAMA_SERVER_URL = "http://localhost:11434/api/chat"
 
-SYSTEM_PROMPT = """you are johnny, you like pizza. DO NOT TALK ABOUT POEKEMON!"""
-
-
-
+#SYSTEM_PROMPT = """you are johnny, you like pizza. DO NOT TALK ABOUT POEKEMON!"""
 
 # In-memory dictionary to store user and assistant messages
 # I have left this in for Stephen to examine, memory functionality will be removed from Flask app1
 memory = {}
 
-
-def payload():
-    return jsonify({
-    "name": "", #DisplayName from Flutter App
-    "vocab_complexity": None,  #int
-    "tone": "data", #int
-    "token_length": "data", #int
-    "message": "data", 
-    "context": [
-        {"role": "user", "message": "Hello"},
-        {"role": "assistant", "message": "Hi there!"}
-    ]
-    }), 201
-
-@app.route('/hello', methods=['POST', 'GET'])
-def hello():
-    print("chur")
-    return payload()
 
 
 #This function accepts a message in the body of a POST request
@@ -51,12 +28,9 @@ def hello():
 #The message and llama reply are added to memory dictionary
 @app.route("/safe_chat", methods=["POST"])
 def safe_chat():
-    user_message = request.json.get("message", "")
-    context = request.json.get("context", "") #for context(memory) passed in via POST body
-
-    print(context)
+    user_message = request.json.get("message", "") #This is the user prompt
+    context = request.json.get("context", "") #for context (memory) passed in via POST body
     
-
     #If message is not in payload error code is returned
     if not user_message:
         return jsonify({"error": "Message is required"}), 400
@@ -67,7 +41,11 @@ def safe_chat():
     payload = {
         "model": "llama3.2",
         "messages": [{"role": "system", "content": SYSTEM_PROMPT}] + context,
-        "stream": False
+        "stream": False,
+        "tempurature" : 0.0, #Very stict hallucintaion control
+        "top_p" : 1.0, #Every token is considered, strict hallucintaion control
+        "num_predict" : token_length[0],
+        "stop" : ["</s>"] #This is Meta's recommendation to stop hallucinated user messages
     }
 
     try:
@@ -91,24 +69,6 @@ def home():
 
 
 
-
-
-
-@app.route('/chat', methods = ['POST'])
-def chat():
-
-
-    try:
-        data = request.get_json()
-        name = data.get('name')
-        message = data.get('message')
-        return {"response" : "hi it works", "name" : name, "message" : message}
-    except:
-        return jsonify({"response" : "no good"}), 420
-
-
-
 if __name__ == '__main__':
-    print("hello")
     app.run(debug=True)
-    
+
