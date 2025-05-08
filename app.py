ebc362e0c0059d13bae062515897f76d88550fff
d-beech <datubeechmusic@gmail.com>
2025-05-09 10:53:44 +1200

    Added basic docx file reading and summarization functions

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request,  jsonify
 import requests
+import docx
+
 
 app = Flask(__name__)
 
@@ -9,7 +11,7 @@ tone = ('friendly', 'extremely aggressive', 'formal')
 
 
 SYSTEM_PROMPT =f'''You are an educational chatbot called Juan, respond using a {tone[0]} tone. 
-#Respond using {vocab_complexity[2]} vocabulary. Do not talk about Pokemon'''
+#Respond using {vocab_complexity[2]} vocabulary. Do not talk about Pokemon. Give very consise responses only'''
 
 llama_endpoint = "http://127.0.0.1:11434/api/chat"
 
@@ -67,7 +69,50 @@ def safe_chat():
 def home():
     return render_template('index.html')
 
+def extract_text_from_docx(docx_file):
+    doc = docx.Document(docx_file)
+    text = []
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+    return "\n".join(text)
 
+@app.route('/fileTest', methods=['GET'])
+def fileTest():
+
+    message = "please summarize the following: "
+
+    with open('CS301.2_IDD_Datu_Beech_Submission.docx', 'rb') as file:
+        text = extract_text_from_docx(file)  # Adjust 40 if needed
+        message += text
+
+    print(message)
+    
+     # Prepare payload with history
+    payload = {
+        "model": "llama3.2",
+        "messages": [{"role": "system", "content": SYSTEM_PROMPT}] + [{"role":"user", "content" : message}],
+        "stream": False,
+        "temperature" : 0.0, #Very stict hallucintaion control
+        "top_p" : 1.0, #Every token is considered, strict hallucintaion control
+        "num_predict" : token_length[0],
+        "stop" : ["</s>"] #This is Meta's recommendation to stop hallucinated user messages
+    }
+
+    try:
+        response = requests.post(LLAMA_SERVER_URL, json=payload)
+        response.raise_for_status()
+        data = response.json()
+
+        # Get model's reply
+        model_reply = data.get("message", {}).get("content", "No response from model.")
+
+        return jsonify({"response": model_reply}), 200
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+    return jsonify({"content" : text})
 
 if __name__ == '__main__':
     app.run(debug=True)
