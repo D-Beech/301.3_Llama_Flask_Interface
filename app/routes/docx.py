e2560e3f6b5e094beeb2adeb4fbaba0694980ea5
D-Beech <135070206+D-Beech@users.noreply.github.com>
2025-06-20 12:54:42 +1200

    For submission, added comments, tidied up

--- app/routes/docx.py
+++ app/routes/docx.py
@@ -4,6 +4,7 @@ from app.utils.summarizer import generate, generate_no_stream
 from botocore.exceptions import NoCredentialsError
 import os
 import app.utils.custom_guards as cg
+from app.firebase_auth import require_firebase_auth, g
 
 docx_bp = Blueprint('docx', __name__)
 
@@ -11,6 +12,7 @@ docx_bp = Blueprint('docx', __name__)
 #Reads file from S3 using file_key
 #Summarises document and returns, summary + original text    
 @docx_bp.route('/process-file', methods=['POST'])
+@require_firebase_auth
 def process_file():
     file_key = request.json.get('file_key')
     print("file_key:", file_key)
@@ -34,9 +36,6 @@ def process_file():
 
         sys_prompt = "Summarize the following document text clearly and helpfully. Do not make up information under any circumstance"
 
-        #testing
-        return jsonify({"og_text" : text})
-
         return jsonify({"summary" : generate_no_stream(text, [], sys_prompt), "og_text" : text})
     except Exception as e:
         print("Error during /process-docx:", str(e))
@@ -45,6 +44,7 @@ def process_file():
 #Generates pre-signed url for document upload from client app
 #This method was chosen to minimize load on server
 @docx_bp.route('/generate-upload-url', methods=['POST'])
+@require_firebase_auth
 def generate_upload_url():
     data = request.get_json()
     filename = data.get('filename')
